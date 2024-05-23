547b9cec2517eb39f945452ed44b83d843ad8b06
me <me@eample.com>
2024-05-24 03:57:05 +1000

    add my photo

--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -5,70 +5,71 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 
 import {
-  slideInFromLeft,
-  slideInFromRight,
-  slideInFromTop,
+    slideInFromLeft,
+    slideInFromRight,
+    slideInFromTop,
 } from "@/lib/motion";
 
 export const HeroContent = () => {
-  return (
-    <motion.div
-      initial="hidden"
-      animate="visible"
-      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
-    >
-      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+    return (
         <motion.div
-          variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]]"
+            initial="hidden"
+            animate="visible"
+            className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
         >
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
-            BlockChain Developer Portfolio
-          </h1>
+            <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+                <motion.div
+                    variants={slideInFromTop}
+                    className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]]"
+                >
+                    <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+                    <h1 className="Welcome-text text-[13px]">
+                        BlockChain Developer Portfolio
+                    </h1>
+                </motion.div>
+
+                <motion.div
+                    variants={slideInFromLeft(0.5)}
+                    className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
+                >
+                    <span>
+                        Providing{" "}
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+                            the best Blockchain
+                        </span>{" "}
+                        development experience.
+                    </span>
+                </motion.div>
+
+                <motion.p
+                    variants={slideInFromLeft(0.8)}
+                    className="text-lg text-gray-400 my-5 max-w-[600px]"
+                >
+                    I&apos;m a Blockchain and Solana Bot Software Engineer with
+                    experience over 5 years. Check out my projects and skills.
+                </motion.p>
+
+                <motion.a
+                    variants={slideInFromLeft(1)}
+                    className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+                >
+                    Learn more
+                </motion.a>
+            </div>
+
+            <motion.div
+                variants={slideInFromRight(0.8)}
+                className="w-full h-full flex justify-center items-center"
+            >
+                <Image
+                    src="/Andy.png"
+                    alt="work icons"
+                    height={550}
+                    width={400}
+                    draggable={false}
+                    className="select-none"
+                />
+            </motion.div>
         </motion.div>
-
-        <motion.div
-          variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
-        >
-          <span>
-            Providing{" "}
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              the best Blockchain
-            </span>{" "}
-            development experience.
-          </span>
-        </motion.div>
-
-        <motion.p
-          variants={slideInFromLeft(0.8)}
-          className="text-lg text-gray-400 my-5 max-w-[600px]"
-        >
-          I&apos;m a Blockchain and Solana Bot Software Engineer with experience over 5 years. Check out my projects and skills.
-        </motion.p>
-
-        <motion.a
-          variants={slideInFromLeft(1)}
-          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
-        >
-          Learn more
-        </motion.a>
-      </div>
-
-      <motion.div
-        variants={slideInFromRight(0.8)}
-        className="w-full h-full flex justify-center items-center"
-      >
-        <Image
-          src="/hero-bg.svg"
-          alt="work icons"
-          height={650}
-          width={650}
-          draggable={false}
-          className="select-none"
-        />
-      </motion.div>
-    </motion.div>
-  );
+    );
 };
